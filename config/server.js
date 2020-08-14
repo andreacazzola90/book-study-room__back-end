2a90a96a463cd88ddd2bd2da2bc1a48597a93af8
andrea <andreacazzola90@gmail.com>
2020-08-14 11:28:32 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e829432af9a0b687da3a840e4c36c878'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cdd3404a-9ed3-453a-ad4e-7fba91638287'
+};
